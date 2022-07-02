f0a6965d9c7745349923de0dad771331066f35d9
openbsod <ispravnikov.spb@gmail.com>
2022-07-02 11:28:48 +0300

    first bad version

--- first_bad_version.py
+++ first_bad_version.py
@@ -1,11 +1,11 @@
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        start = 1
-        end = n
-        while start <= end:
-            mid = (start + (end-start))
-            if isBadVersion(mid):
-                end = mid - 1
-            else:
-                start = mid + 1
-        return start
+        def binSearch(p, q):
+            if p < q:
+                mid = (p + q) // 2
+                if isBadVersion(mid):
+                    return binSearch(p, mid)
+                else:
+                    return binSearch(mid + 1, q)
+            return p
+        return binSearch(1, n)
